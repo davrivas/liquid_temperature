8b1fcf99f0d34971e3d450b99547bdf3e9ac571c
David Rivas <david.rivas@pevaar.com>
2021-07-21 22:42:16 -0500

    Main logic of the project except the api communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from max6675 import MAX6675
-from machine import I2C, Pin
+from machine import I2C, PWM, Pin
 from ssd1306 import SSD1306_I2C
 import time
 
@@ -9,26 +9,55 @@ height = 64
 i2c = I2C(0, scl = Pin(22), sda = Pin(21))
 oled = SSD1306_I2C(width, height, i2c)
 
-def main():
-    sck = 15
-    cs = 2    
-    so = 4
-    unit = 1
+buzzer = PWM(Pin(32), freq = 500)
+buzzer.duty(0) # don't produce a sound
 
-    thermocuple = MAX6675(cs, sck, so, unit) # [unit : 0 - raw, 1 - Celsius, 2 - Fahrenheit]
+blue_led = Pin(18, Pin.OUT)
+red_led = Pin(19, Pin.OUT)
+
+sck = 15
+cs = 2    
+so = 4
+thermocouple = MAX6675(cs, sck, so)
+
+def main():
+    
     
     while True:
         #clear the screen
         oled.fill(0)
         
-        # read temperature connected at CS 22
-        temp = float(thermocuple.read_temp())
+        # read temperature
+        temp = thermocouple.read_temp()
         
-        text = "T = {}C".format(temp)
+        text = "{:.2f}C".format(temp)
         
         # show temperature
-        oled.text(text, 0, 10)
-        print(text)
+        oled.text("-" * 16, 0, 10)
+        oled.text("Temperature:", 0, 20)
+        oled.text(text, 0, 30)
+        print("Temperature", text)
+        
+        if (temp >= 90): # if temperature is 90 or greater, turn on red led and play a sound for half second
+            red_led.on()
+            blue_led.off()
+            buzzer.duty(5)
+            time.sleep(.5)
+            buzzer.duty(0)
+            red_led.off()
+            oled.text("PLEASE STOP!!!", 0, 40)
+        elif (temp > 75): # if temperature is greater than 75, turn on blue led
+            red_led.off()
+            blue_led.on()
+            buzzer.duty(0)
+            oled.text("HIGH TEMPERATURE", 0, 40)
+        else:
+            blue_led.off()
+            red_led.off()
+            buzzer.duty(0)
+            oled.text("OK", 0, 40)
+            
+        oled.text("-" * 16, 0, 50)
         oled.show()
 
         # when there are some errors with sensor, it return "-" sign and CS pin number
